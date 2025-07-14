8a61489817cbad6de938f40bd6041d32d55f3aff
Xurshidbek003 <xurshidbekt571@gmail.com>
2025-07-14 15:19:13 +0500

    second commet

--- db.py
+++ db.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine('mysql+pymysql://root@localhost:3306/database_name')
+engine = create_engine('mysql+pymysql://root:umar@localhost:3306/fastapi_oauth')
 SessionLocal = sessionmaker(bind=engine)
 
 
