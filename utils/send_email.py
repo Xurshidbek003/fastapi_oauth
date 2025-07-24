2c9fda3a0dd074148a05b0c6654bcb67b990099e
Nurzotbek0303 <nurzotbeke@gmail.com>
2025-07-24 19:19:17 +0500

    sadsf

--- utils/send_email.py
+++ utils/send_email.py
@@ -1,4 +1,4 @@
-from fastapi import  HTTPException
+from fastapi import HTTPException
 from pydantic import BaseModel, EmailStr
 import smtplib
 from email.mime.text import MIMEText
@@ -6,7 +6,7 @@ from email.mime.multipart import MIMEMultipart
 
 SMTP_SERVER = "smtp.gmail.com"
 SMTP_PORT = 587
-EMAIL_ADDRESS = "xurshidbekt1020@gmail.com"
+EMAIL_ADDRESS = "xurshidbekt@gmail.com"
 EMAIL_PASSWORD = "hsqx xccq rbnt ewwl"
 
 
@@ -29,7 +29,10 @@ def send_email(recipient: str, subject: str, message: str):
             server.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
             server.sendmail(EMAIL_ADDRESS, recipient, msg.as_string())
     except smtplib.SMTPAuthenticationError:
-        raise HTTPException(status_code=500, detail="SMTP Authentication failed. Please check your credentials.")
+        raise HTTPException(
+            status_code=500,
+            detail="SMTP Authentication failed. Please check your credentials.",
+        )
     except smtplib.SMTPException as e:
         raise HTTPException(status_code=500, detail=f"SMTP error occurred: {str(e)}")
     except Exception as e:
